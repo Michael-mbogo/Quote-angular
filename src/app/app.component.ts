47e39a4f7b784aaceda9699dbe21e936c538be4a
Michael-mbogo <mikembogo17@gmail.com>
2018-05-28 14:45:24 +0300

    Add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Quote } from './goal'
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,9 @@ import { Quote } from './goal'
 })
 export class AppComponent {
   title = 'Quotes';
+
+  quotes=[
+    new Quote(1, 'I just finally discovered whats wrong with my brain on the left side there is nothing right and on the right side, there is nothing left'),
+    new Quote(1, 'Me and my bed are perfect for each other, but my alarm clock keeps trying to break us up.'),
+  ]
 }
