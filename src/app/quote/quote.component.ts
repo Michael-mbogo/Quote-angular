fcaf54e8856f53a67bde67309d72f3bdd099f8b0
Michael-mbogo <mikembogo17@gmail.com>
2018-05-28 16:29:42 +0300

    Add delete button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,10 +12,16 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'I just finally discovered whats wrong with my brain on the left side there is nothing right and on the right side, there is nothing left.','By Michael.m'),
     new Quote(1, 'Me and my bed are perfect for each other, but my alarm clock keeps trying to break us up.','By Anderson.NJ'),
     new Quote(1, 'I changed my password everywhere to incorrect. That way when I forget it, it always reminds me.','By MattKinson'),
-  ]quo
+  ]
 
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription
+  }
+
+  completeQuote(isComplete,index){
+    if(isComplete){
+      this.quotes.splice(index,1)
+    }
   }
 
 
