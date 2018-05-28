1a8f33bfcd50bdfd8d5992e29a3ec161251b9046
Michael-mbogo <mikembogo17@gmail.com>
2018-05-28 15:49:15 +0300

    Add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,4 @@ import { Quote } from './quote'
 })
 export class AppComponent {
   title = 'Quotes';
-
-  quotes=[
-    new Quote(1, 'I just finally discovered whats wrong with my brain on the left side there is nothing right and on the right side, there is nothing left'),
-    new Quote(1, 'Me and my bed are perfect for each other, but my alarm clock keeps trying to break us up.'),
-  ]
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
 
-  quotes=[
-    new Quote(1, 'I just finally discovered whats wrong with my brain on the left side there is nothing right and on the right side, there is nothing left','By Michael.m'),
-    new Quote(2, 'Me and my bed are perfect for each other, but my alarm clock keeps trying to break us up.','By Anderson.NJ'),
-    new Quote(3, 'I changed my password everywhere to incorrect. That way when I forget it, it always reminds me.','By MattKinson'),
+  quotes =[
+    new Quote(1, 'I just finally discovered whats wrong with my brain on the left side there is nothing right and on the right side, there is nothing left.','By Michael.m'),
+    new Quote(1, 'Me and my bed are perfect for each other, but my alarm clock keeps trying to break us up.','By Anderson.NJ'),
+    new Quote(1, 'I changed my password everywhere to incorrect. That way when I forget it, it always reminds me.','By MattKinson'),
   ]
 
   constructor() { }
