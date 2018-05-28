c2313c16d61669c761076b793172e7951e59a70b
Michael-mbogo <mikembogo17@gmail.com>
2018-05-28 16:51:38 +0300

    Add delete btn

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -18,9 +18,15 @@ export class QuoteComponent implements OnInit {
     this.quotes[index].showDescription = !this.quotes[index].showDescription
   }
 
-  completeQuote(isComplete,index){
+  deleteQuote(isComplete,index){
     if(isComplete){
-      this.quotes.splice(index,1)
+      let toDelete = confirm('Are you sure you want to delelte $(this.quotes[index].name)')
+
+      if(toDelete){
+        this.quotes.splice(index,1)
+      }
+
+
     }
   }
 
